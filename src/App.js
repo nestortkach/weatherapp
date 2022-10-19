7323c1f94a1ca64bd1acc1f55838158fd9e11b82
nestortkach <nestor.tkach17@gmail.com>
2022-10-19 11:14:51 +0300

    main page was created

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      weather app
+      <div className="App_main">
+        <h1 className="App_title">
+          Weather Forecast
+        </h1>
+        <h2 className="App_description">
+          Enter a zipcode below to get the current weather conditions for that area.
+        </h2>
+        <div className="App_form">
+          <input
+            type="text"
+            placeholder="Enter zipcode..."
+            className="App_input"
+          />
+          <div className="App_submit">
+            ENTER
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
